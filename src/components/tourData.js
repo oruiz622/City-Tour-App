8a882a040897e50af893137f968ea6ce3abbd76e
oruiz622 <oruiz622@gmail.com>
2020-05-25 13:40:01 -0400

    Firt and Final Commit

---
+++ src/components/tourData.js
@@ -0,0 +1,58 @@
+export const tourData = [
+  {
+    id: 1,
+    city: 'new york',
+    img: './img/newyork.jpeg',
+    name: 'new york bridge tour',
+    info:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!'
+  },
+  {
+    id: 2,
+    city: 'paris',
+    img: './img/paris.jpeg',
+    name: 'paris lights tour',
+    info:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!'
+  },
+  {
+    id: 3,
+    city: 'london',
+    img: './img/london.jpeg',
+    name: 'london royal palace tour',
+    info:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!'
+  },
+  {
+    id: 4,
+    city: 'tokyo',
+    img: './img/tokyo.jpeg',
+    name: 'tokyo sushi tour',
+    info:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!'
+  },
+  {
+    id: 5,
+    city: 'Madrid',
+    img: './img/city.jpeg',
+    name: 'Beautiful City',
+    info:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!'
+  },
+  {
+    id: 6,
+    city: 'Athens',
+    img: './img/cloud-city.jpeg',
+    name: 'City in the morning',
+    info:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!'
+  },
+  {
+    id: 7,
+    city: 'Peking',
+    img: './img/city-purp.jpeg',
+    name: 'Purple city',
+    info:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!'
+  }
+];
